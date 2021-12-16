3e4628427faaf14a4a3dc8868c47733507f54a11
otayfuroglu <tayfuroglu@gmail.com>
2021-12-16 11:31:33 +0300

    aded package data

--- setup.py
+++ setup.py
@@ -2,7 +2,8 @@ from setuptools import find_packages, setup
 
 setup(
     name="aimnet",
-    packages=find_packages(include=["aimnet"]),
+    packages=find_packages(),
+    include_package_data=True,
     version="0.1",
     description="aimnet models and calculator",
     author="otayfuroglu",
